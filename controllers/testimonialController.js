3ba266c0efc47755f7273bd76542260325f247de
suhani chauhan <suhani.chauhan@campus.dsvv.in>
2025-07-09 10:18:58 +0530

    Admin can edit and delete testimonial

--- controllers/testimonialController.js
+++ controllers/testimonialController.js
@@ -9,16 +9,16 @@ exports.submitTestimonial = async (req, res) => {
       name,
       course,
       text,
-      photo: req.file ? req.file.path : "https://cdn0.iconfinder.com/data/icons/kameleon-free-pack-rounded/110/Student-3-512.png",
-      approved: false, 
+      photo: req.file
+        ? req.file.path
+        : "https://cdn0.iconfinder.com/data/icons/kameleon-free-pack-rounded/110/Student-3-512.png",
+      approved: false,
     });
 
     await newTestimonial.save();
-    res
-      .status(201)
-      .json({
-        message: "Testimonial submitted successfully, pending approval!",
-      });
+    res.status(201).json({
+      message: "Testimonial submitted successfully, pending approval!",
+    });
   } catch (err) {
     console.error("[CREATE TESTIMONIAL ERROR]", err);
     res.status(500).json({ error: "Failed to submit testimonial" });
@@ -47,6 +47,49 @@ exports.approveTestimonial = async (req, res) => {
     res.status(500).json({ error: "Failed to approve testimonial." });
   }
 };
+
+// Edit testimonial
+exports.updateTestimonial = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { name, course, text } = req.body;
+
+    const testimonial = await Testimonial.findById(id);
+    if (!testimonial)
+      return res.status(404).json({ error: "Testimonial not found" });
+
+    testimonial.name = name || testimonial.name;
+    testimonial.course = course || testimonial.course;
+    testimonial.text = text || testimonial.text;
+
+    await testimonial.save();
+
+    res.status(200).json({ message: "Testimonial updated successfully" });
+  } catch (err) {
+    console.error("[UPDATE TESTIMONIAL ERROR]", err);
+    res.status(500).json({ error: "Failed to update testimonial" });
+  }
+};
+
+// Reject/delete testimonial
+exports.deleteTestimonial = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const testimonial = await Testimonial.findById(id);
+    if (!testimonial) {
+      return res.status(404).json({ error: "Testimonial not found" });
+    }
+
+    await testimonial.deleteOne();
+
+    res.status(200).json({ message: "Testimonial rejected and deleted successfully" });
+  } catch (err) {
+    console.error("[DELETE TESTIMONIAL ERROR]", err);
+    res.status(500).json({ error: "Failed to delete testimonial" });
+  }
+};
+
 
 // Get approved testimonials for homepage
 exports.getApprovedTestimonials = async (req, res) => {
--- routes/testimonialRoutes.js
+++ routes/testimonialRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const {submitTestimonial,getPendingTestimonials,approveTestimonial,getApprovedTestimonials} = require("../controllers/testimonialController");
+const {submitTestimonial,getPendingTestimonials,approveTestimonial,getApprovedTestimonials, updateTestimonial, deleteTestimonial} = require("../controllers/testimonialController");
 const { protect, adminOnly } = require("../middleware/authMiddleware");
 const multer = require("multer");
 const { storage } = require("../config/cloudinary");
@@ -9,6 +9,8 @@ const upload = multer({ storage });
 router.post("/", upload.single("photo"),submitTestimonial);
 router.get("/pending", protect, adminOnly, getPendingTestimonials);
 router.put("/:id/approve", protect, adminOnly, approveTestimonial);
+router.put("/:id", protect, adminOnly, updateTestimonial);     
+router.delete("/:id", protect, adminOnly, deleteTestimonial);
 router.get("/approved", getApprovedTestimonials);
 
 module.exports = router;
